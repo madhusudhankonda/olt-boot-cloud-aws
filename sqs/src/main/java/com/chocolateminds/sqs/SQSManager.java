3b6de7d782075fb0166f9b075077dc2b9ce2a6dc
M Konda <m.konda@outlook.com>
2021-04-21 21:47:46 +0100

    SQS using AWSpring 2.3.1, Spring boot 2.4.5 and AWS SDK 1.11

---
+++ sqs/src/main/java/com/chocolateminds/sqs/SQSManager.java
@@ -0,0 +1,50 @@
+package com.chocolateminds.sqs;
+
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
+import io.awspring.cloud.messaging.listener.annotation.SqsListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SQSManager implements CommandLineRunner {
+
+    private final String QUEUE_NAME = "my-sample04044";
+
+    private QueueMessagingTemplate queueMessagingTemplate;
+
+    @Autowired
+    public SQSManager(AmazonSQSAsync amazonSQSAsync){
+        queueMessagingTemplate = new QueueMessagingTemplate(amazonSQSAsync);
+    }
+
+    public void sendMessage(String message){
+        System.out.println("Sending a message"+message);
+        queueMessagingTemplate.convertAndSend(QUEUE_NAME, message);
+    }
+
+    /**
+     * Receive a message using a listener
+     * @param message
+     */
+    @SqsListener(QUEUE_NAME)
+    public void receiveMessage(String message){
+        System.out.println("Received message"+message);
+    }
+
+    /**
+     * Receive a message by specifying a queuename on the template
+     * @param queName
+     */
+    public void receive(String queName){
+        Message<?> message = queueMessagingTemplate.receive(queName);
+        System.out.println(">>"+message.getPayload());
+    }
+    @Override
+    public void run(String... args) throws Exception {
+        sendMessage("Hello, SQS World!");
+    //  receive(QUEUE_NAME);
+    }
+}
---
+++ sqs/src/test/java/com/chocolateminds/sqs/SqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chocolateminds.sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
