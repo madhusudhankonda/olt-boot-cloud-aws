aad7835474199e9ba51854d341b9ae21957c9d61
madhusudhankonda <m.konda@outlook.com>
2021-04-22 11:03:16 +0100

    S3 additional method invocations

--- s3/src/main/java/com/chocolateminds/s3/S3Manager.java
+++ s3/src/main/java/com/chocolateminds/s3/S3Manager.java
@@ -1,9 +1,12 @@
 package com.chocolateminds.s3;
 
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.S3Object;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class S3Manager implements CommandLineRunner {
@@ -18,8 +21,32 @@ public class S3Manager implements CommandLineRunner {
     public void createBucket(String bucketName){
         amazonS3Client.createBucket(bucketName);
     }
+
+    private void deleteBucket(String bucketName){
+        amazonS3Client.deleteBucket(bucketName);
+    }
+    private void listBuckets(){
+        amazonS3Client.listBuckets().stream().forEach(System.out::println);
+    }
+
+    private void downloadFile(String bucketName, String fileName){
+        S3Object object = amazonS3Client.getObject(bucketName,fileName);
+        System.out.println(object);
+    }
+
+    private void uploadFile(String bucketName, String fileName, File file) {
+        amazonS3Client.putObject(bucketName,fileName,file);
+    }
+
+
     @Override
     public void run(String... args) throws Exception {
-        createBucket("bootcloudaws-8976");
+        String bucketName = "bootcloudaws-8976";
+        String fileName = "products.json";
+//        createBucket("bootcloudaws-8976");
+        File file = new File(fileName);
+        uploadFile(bucketName,fileName, file);
+        listBuckets();
+        deleteBucket("temp-me/s3-resource-loader.iml");
     }
 }
